3452604afcc2a8516d128d9d7b025ad84ab96bbc
Kelechi Ngaka <kelechi.charles@gmail.com>
2016-04-20 15:54:50 -0400

    Added Rate This App to MovieActivity

--- app/src/main/java/com/serigon/moviezy/activity/MovieActivity.java
+++ app/src/main/java/com/serigon/moviezy/activity/MovieActivity.java
@@ -1,6 +1,8 @@
 package com.serigon.moviezy.activity;
 
 
+import android.content.ActivityNotFoundException;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
@@ -12,11 +14,6 @@ import android.view.MenuItem;
 import android.widget.GridView;
 
 import com.serigon.movietrend.R;
-import com.serigon.moviezy.activity.DetailActivity;
-import com.serigon.moviezy.activity.DetailActivityFragment;
-import com.serigon.moviezy.activity.IntroActivity;
-import com.serigon.moviezy.activity.MovieFragment;
-import com.serigon.moviezy.activity.SettingsActivity;
 
 public class MovieActivity extends AppCompatActivity implements MovieFragment.Callback {
 
@@ -66,7 +63,22 @@ public class MovieActivity extends AppCompatActivity implements MovieFragment.Ca
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_rate) {
-            startActivity(new Intent(this, SettingsActivity.class));
+            Context context = getApplicationContext();
+
+            Uri uri = Uri.parse("market://details?id=" + context.getPackageName());
+            Intent goToMarket = new Intent(Intent.ACTION_VIEW, uri);
+            // To count with Play market backstack, After pressing back button,
+            // to taken back to our application, we need to add following flags to intent.
+            goToMarket.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY |
+                    Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET |
+                    Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
+
+            try { startActivity(goToMarket); }
+            catch (ActivityNotFoundException e) {
+                startActivity(new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("http://play.google.com/store/apps/details?id=" + context.getPackageName())));
+            }
+
             return true;
         }
 
